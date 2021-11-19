de35815a68ebaeae7036605551639d839bfb72d4
Ag-2O <sakuramoti3079@gmail.com>
2021-11-19 14:35:23 +0900

    kaiseki

--- read_data.py
+++ read_data.py
@@ -11,14 +11,15 @@ import subprocess
 from hsreplay.document import HSReplayDocument as hsd
 import hsreplay.utils as hsu
 import hslog
+import xml.etree.ElementTree as ET
 
 # xmlファイルを解読する
 
 # 見やすくして保存
 def annotate():
     path = os.getcwd()
-    xml_file = path + "/replay/test.xml"
-    read_file = path + "/read/test.xml"
+    xml_file = path + "/replay/log_20211117_023415.xml"
+    read_file = path + "/read/out_20211117_023415.xml"
 
     # アノテーション
     game = hsd.from_xml_file(xml_file)
@@ -27,8 +28,28 @@ def annotate():
 # 必要なデータの取り出し
 def get_step():
     # 1step毎のデータを取り出す
+    path = os.getcwd()
+    xml = path + "/read/out_20211117_023415.xml"
+
+    # xmlファイルの解析
+    tree = ET.parse(xml)
+    root = tree.getroot()
+
+    print("tag: {}".format(root.tag))
+    print("attrib: {}".format(root.attrib))
+
+    child = root[0]
+    print("tag: {}".format(child.tag))
+    print("attrib: {}".format(child.attrib))
+
+    for ch in child.findall("Block"):
+        print("Block : {}".format(ch.attrib))
+        for c in ch.findall("TagChange"):
+            print("    TagChange : {}".format(c.attrib))
+        print("\n")
+
     pass
 
-
 if __name__ == "__main__":
-    annotate()
+    #annotate()
+    get_step()
