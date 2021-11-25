024301091d9e56dce7b72eefbc95bc9e1e3087b7
Ag-2O <sakuramoti3079@gmail.com>
2021-11-26 03:17:55 +0900

    add get_state

--- get_data.py
+++ get_data.py
@@ -95,6 +95,30 @@ def move(mode=True,num=10):
     driver.quit()
 
 def move_replay(wait):
+    # 行動履歴の取得
+    get_history(wait)
+
+    # プレイヤーの状態の取得
+    get_player_state(wait)
+
+    # 手札の状態の取得
+    get_hand_state(wait)
+
+    # 盤面の状態の取得
+    get_board_state(wait)
+
+    # 状態の比較
+    if compare_state(wait):
+        # 直前の状態と比較して同じならスルー
+        pass
+    else:
+        # 直前の状態と比較して異なるなら保存???
+        save_state()
+
+    pass
+
+# 行動履歴の取得
+def get_history(wait):
     # 設定を開く
     scrubber = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'joust-scrubber')))
     button = scrubber.find_elements_by_tag_name("button")[3]
@@ -111,26 +135,84 @@ def move_replay(wait):
     log_bar = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'joust-log')))
     log = log_bar.find_elements_by_xpath("//div[@class='joust-log']/div")
     print("log: {}".format(log))
-    get_replay(wait,log)
-
-    pass
-
-def get_replay(wait,log):
-    # ゲームの再生
-    g = game.Game()
-
-    # todo: 行動履歴を基にゲームを再生して、状態を取る
-    # todo: 相手のカードは伏せる
-    # todo: プレイヤー名をどっかからか取得する playerとopponent
-    # todo: カードのデータは他のxmlファイルから取得してくる
-    # todo: 
-
-
-
     for l in log:
         print("action: {}".format(l.get_attribute("textContent")))
 
+# 両プレイヤーの状態
+def get_player_state(wait):
+    # プレイヤーの状態の取得
+    game = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'game')))
+    player_info = game.find_elements_by_xpath("//*[@id='joust-container']/div/div[1]/div[1]/div[2]/section[2]/div/section[1]/div/div[2]")
     
+    for pi in player_info:
+        # 攻撃力(武器持ってたり)
+        atk_list = pi.find_elements_by_class_name("atk")
+        if atk_list == []:
+            player_atk = 0
+        else:
+            player_atk = atk_list[0].get_attribute("textContent")
+
+        # 体力
+        health_list = pi.find_elements_by_class_name("health")
+        if health_list == []:
+            health_nega_list = pi.find_elements_by_class_name("health.negative")
+            if health_nega_list == []:
+                player_health = health_nega_list[0].get_attribute("textContent")
+        else:
+            player_health = health_list[0].get_attribute("textContent")
+        
+        # マナ
+        mana_list = pi.find_elements_by_xpath("//*[@id='joust-container']/div/div[1]/div[1]/div[2]/section[2]/div/section[2]/div[2]/span")
+        player_current_mana = mana_list[0].get_attribute("textContent")[0]
+        player_max_mana = mana_list[0].get_attribute("textContent")[2]
+    
+    print("player_info: health={} atk={} mana={}/{}".format(player_health,player_atk,player_current_mana,player_max_mana))
+    
+    # 敵の状態の取得
+    opponent_info = game.find_elements_by_xpath("//*[@id='joust-container']/div/div[1]/div[1]/div[1]/section[1]/div/section[1]/div/div[2]")
+    
+    for oi in opponent_info:
+        # 攻撃力(武器持ってたり)
+        op_atk_list = oi.find_elements_by_class_name("atk")
+        if op_atk_list == []:
+            opponent_atk = 0
+        else:
+            opponent_atk = atk_list[0].get_attribute("textContent")
+
+        # 体力
+        op_health_list = oi.find_elements_by_class_name("health")
+        if op_health_list == []:
+            op_health_nega_list = oi.find_elements_by_class_name("health.negative")
+            if op_health_nega_list == []:
+                opponent_health = op_health_nega_list[0].get_attribute("textContent")
+        else:
+            opponent_health = op_health_list[0].get_attribute("textContent")
+        
+        # マナ
+        op_mana_list = pi.find_elements_by_xpath("//*[@id='joust-container']/div/div[1]/div[1]/div[1]/section[1]/div/section[2]/div[2]/span")
+        opponent_max_mana = op_mana_list[0].get_attribute("textContent")[2]
+    
+    print("opponent_info: health={} atk={} mana={}".format(opponent_health,opponent_atk,opponent_max_mana))
+    
+# 手札の取得
+def get_hand_state(wait):
+    # 手札の状態
+    game = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'game')))
+    hand_state = game.find_element_by_xpath("//*[@id='joust-container']/div/div[1]/div[1]/div[2]/section[2]/ul")
+    hand_cards = hand_state.find_elements_by_tag_name("li")
+    print("hand_cards: {}".format(hand_cards))
+
+def get_board_state(wait):
+    # 盤面の状態
+    pass
+
+def compare_state(wait):
+    # ページ更新の比較
+    # 前と変化していたら値を取得
+    pass
+
+def save_state():
+    # ゲームの状態の保存
     pass
 
 if __name__ == "__main__":
